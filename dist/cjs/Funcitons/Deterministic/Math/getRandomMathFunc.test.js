fb2f450829533f48b0c6af7a36352263e6e8ac3e
l-monninger <liammmonninger@gmail.com>
2021-09-28 10:48:18 -0700

    Looks like I didn't commit the tests on Friday.

--- dist/cjs/Funcitons/Deterministic/Math/getRandomMathFunc.test.js
+++ dist/cjs/Funcitons/Deterministic/Math/getRandomMathFunc.test.js
@@ -1,11 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CheckOrderTestSuiteA = void 0;
+const getRandomMathFunc_1 = require("./getRandomMathFunc");
+const determinismus_1 = require("determinismus");
+const tulleries_1 = require("tulleries");
 const CheckOrderTestSuiteA = () => {
     describe("Basic functionality", () => {
-        test("Accurately reports functions ran in order", () => {
+        test("Results from functions are likely deterministic", () => {
+            const i = Math.floor(Math.random() * 100) + 10;
+            Array(i).fill(null).map(() => {
+                const func = (0, getRandomMathFunc_1.getRandomMathFunciton)();
+                expect((0, determinismus_1.seemsDeterministic)({
+                    func: func,
+                    argGenerator: getRandomMathFunc_1.generateRandomMathArgs,
+                    toleranceFunc: (0, tulleries_1.withPrecision)()
+                })).toBe(true);
+            });
         });
-        test("Accurately reports functions did not run in order", () => {
+        test("Functions picked are likely undeterministic", () => {
+            const i = Math.floor(Math.random() * 100) + 10;
+            Array(i).fill(null).map(() => {
+                expect((0, determinismus_1.seemsDeterministic)({
+                    func: getRandomMathFunc_1.getRandomMathFunciton,
+                    argGenerator: () => [],
+                    toleranceFunc: tulleries_1.functionsStringEqual
+                })).toBe(false);
+            });
         });
     });
 };
--- dist/mjs/Funcitons/Deterministic/Math/getRandomMathFunc.test.js
+++ dist/mjs/Funcitons/Deterministic/Math/getRandomMathFunc.test.js
@@ -1,8 +1,28 @@
+import { getRandomMathFunciton, generateRandomMathArgs } from "./getRandomMathFunc";
+import { seemsDeterministic } from "determinismus";
+import { withPrecision, functionsStringEqual } from "tulleries";
 export const CheckOrderTestSuiteA = () => {
     describe("Basic functionality", () => {
-        test("Accurately reports functions ran in order", () => {
+        test("Results from functions are likely deterministic", () => {
+            const i = Math.floor(Math.random() * 100) + 10;
+            Array(i).fill(null).map(() => {
+                const func = getRandomMathFunciton();
+                expect(seemsDeterministic({
+                    func: func,
+                    argGenerator: generateRandomMathArgs,
+                    toleranceFunc: withPrecision()
+                })).toBe(true);
+            });
         });
-        test("Accurately reports functions did not run in order", () => {
+        test("Functions picked are likely undeterministic", () => {
+            const i = Math.floor(Math.random() * 100) + 10;
+            Array(i).fill(null).map(() => {
+                expect(seemsDeterministic({
+                    func: getRandomMathFunciton,
+                    argGenerator: () => [],
+                    toleranceFunc: functionsStringEqual
+                })).toBe(false);
+            });
         });
     });
 };
